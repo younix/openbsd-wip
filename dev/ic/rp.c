81ce9ff0b7c894fc271457d0db0071ddee385c8d
Jan Klemkow <j.klemkow@wemelug.de>
2020-02-14 17:21:12 +0100

    use markro DEVNAME

--- dev/ic/rp.c
+++ dev/ic/rp.c
@@ -55,6 +55,7 @@
 #define ROCKET_C
 #include <dev/ic/rpreg.h>
 
+#define DEVNAME(_s)	((_s)->sc_dev.dv_xname)
 #define	DEVCUA(x)	(minor(x) & 0x80)
 
 static const char RP_Version[] = "3.02";
@@ -604,8 +605,7 @@ printf("%s:%d while\n", __func__, __LINE__);
 				rp->rp_overflows++;
 
 				printf("%s port %d tty overrun\n",
-				    rp->rp_ctlp->sc_dev.dv_xname,
-				    RP_PORT(tp->t_dev));
+				    DEVNAME(rp->rp_ctlp), RP_PORT(tp->t_dev));
 			}
 
 			//ttydisc_rint(tp, ch, err);
@@ -732,8 +732,8 @@ rp_attach(struct rp_softc *sc, int num_aiops, int num_ports)
 	sc->rp = rp = mallocarray(num_ports, sizeof(*rp), M_DEVBUF, M_NOWAIT|M_ZERO);
 
 	if (rp == NULL) {
-		printf("%s port %d rp_attach: could not malloc\n",
-		    sc->sc_dev.dv_xname, port);
+		printf("%s port %d rp_attach: could not malloc\n", DEVNAME(sc),
+		    port);
 		retval = ENOMEM;
 		goto nogo;
 	}
@@ -826,8 +826,8 @@ rpopen(dev_t dev, int flag, int mode, struct proc *p)
 		return (ENXIO);
 
 #ifdef RP_DEBUG
-	printf("%s open port %d flag 0x%x mode 0x%x\n", sc->sc_dev.dv_xname,
-	    port, flag, mode);
+	printf("%s open port %d flag 0x%x mode 0x%x\n", DEVNAME(sc), port, flag,
+	    mode);
 #endif
 
 	rp = &sc->rp[port];
@@ -953,8 +953,8 @@ rpclose(dev_t dev, int flag, int mode, struct proc *p)
 	int		 s;
 
 #ifdef RP_DEBUG
-	printf("%s close port %d flag 0x%x mode 0x%x\n", sc->sc_dev.dv_xname,
-	    port, flag, mode);
+	printf("%s close port %d flag 0x%x mode 0x%x\n", DEVNAME(sc), port,
+	    flag, mode);
 #endif
 
 	(*linesw[tp->t_line].l_close)(tp, flag, p);
@@ -1017,9 +1017,9 @@ rpread(dev_t dev, struct uio *uio, int flag)
 	struct rp_port	*rp = &sc->rp[port];
 	struct tty	*tp = rp->rp_tty;
 
-#ifdef RP_DEBUG
-	printf("%s read port %d uio %p flag 0x%x\n", sc->sc_dev.dv_xname,
-	    port, uio, flag);
+#ifdef RP_DEBUG1
+	printf("%s read port %d uio %p flag 0x%x\n", DEVNAME(sc), port, uio,
+	    flag);
 #endif
 
 	return ((*linesw[tp->t_line].l_read)(tp, uio, flag));
@@ -1034,9 +1034,9 @@ rpwrite(dev_t dev, struct uio *uio, int flag)
 	struct rp_port *rp = &sc->rp[port];
 	struct tty *tp = rp->rp_tty;
 
-#ifdef RP_DEBUG
-	printf("%s write port %d uio %p flag 0x%x\n", sc->sc_dev.dv_xname,
-	    port, uio, flag);
+#ifdef RP_DEBUG1
+	printf("%s write port %d uio %p flag 0x%x\n", DEVNAME(sc), port, uio,
+	    flag);
 #endif
 
 	return ((*linesw[tp->t_line].l_write)(tp, uio, flag));
@@ -1065,9 +1065,9 @@ rpioctl(dev_t dev, u_long cmd, caddr_t data, int flag, struct proc *p)
 	struct tty	*tp = rp->rp_tty;
 	int error;
 
-#ifdef RP_DEBUG
-	printf("%s port %d ioctl cmd 0x%lx data %p flag 0x%x\n",
-	    sc->sc_dev.dv_xname, port, cmd, data, flag);
+#ifdef RP_DEBUG1
+	printf("%s port %d ioctl cmd 0x%lx data %p flag 0x%x\n", DEVNAME(sc),
+	    port, cmd, data, flag);
 #endif
 
 	error = (*linesw[tp->t_line].l_ioctl)(tp, cmd, data, flag, p);
@@ -1201,8 +1201,8 @@ rpstop(struct tty *tp, int flag)
 	int		 s;
 
 #ifdef RP_DEBUG
-	printf("%s port %d stop tty %p flag 0x%x\n", sc->sc_dev.dv_xname,
-	    port, tp, flag);
+	printf("%s port %d stop tty %p flag 0x%x\n", DEVNAME(sc), port, tp,
+	    flag);
 #endif
 
 	s = spltty();
@@ -1231,9 +1231,8 @@ rpparam(struct tty *tp, struct termios *t)
 //	int		 lflag = t->c_lflag;
 	int		 ospeed = rp_convert_baud(t->c_ispeed);
 
-#ifdef RP_DEBUG
-	printf("%s port %d param tty %p\n", sc->sc_dev.dv_xname,
-	    port, tp);
+#ifdef RP_DEBUG1
+	printf("%s port %d param tty %p\n", DEVNAME(sc), port, tp);
 #endif
 
 #ifdef RPCLOCAL
@@ -1325,8 +1324,8 @@ rpstart(struct tty *tp)
 	int		 i, count, wcount;
 	int		 s;
 
-#ifdef RP_DEBUG
-	printf("%s port %d start, tty %p\n", sc->sc_dev.dv_xname, port, tp);
+#ifdef RP_DEBUG1
+	printf("%s port %d start, tty %p\n", DEVNAME(sc), port, tp);
 #endif
 
 	s = spltty();
